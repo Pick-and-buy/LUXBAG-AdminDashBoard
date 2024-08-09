8aa133e6a17af54319701305853b19bf94697329
nguyentienthanhe150825 <nguyenthanh2542001@gmail.com>
2024-08-09 14:36:10 +0700

    fix: block USER role when entering admin page

--- src/components/ProtectedRoute/NotPermitted.jsx
+++ src/components/ProtectedRoute/NotPermitted.jsx
@@ -1,18 +1,34 @@
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
 import { Button, Result } from "antd";
 
 const NotPermitted = () => {
     const navigate = useNavigate();
+    const user = useSelector(state => state.account.user);
+    const userRole = user.roles[0].name;
 
     return (
-        <Result
-            status="403"
-            title="403"
-            subTitle="Sorry, The page you visited does not exist."
-            extra={
-                <Button type="primary" onClick={() => navigate('/')}>Back Home</Button>
+        <>
+            {userRole === 'ADMIN' ?
+                <Result
+                    status="403"
+                    title="403"
+                    subTitle="Sorry, The page you visited does not exist."
+                    extra={
+                        <Button type="primary" onClick={() => navigate('/')}>Back Home</Button>
+                    }
+                />
+                :
+                <Result
+                    status="403"
+                    title="403"
+                    subTitle="Sorry, The page you visited does not exist."
+                    extra={
+                        <Button type="primary" onClick={() => navigate('/login')}>Return Login</Button>
+                    }
+                />
             }
-        />
+        </>
     )
 }
 
